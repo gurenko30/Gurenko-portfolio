8a2cdc3712b01516fcc2e499b1c10e3bf88341f1
gurenko30 <148851531+gurenko30@users.noreply.github.com>
2023-12-25 03:05:36 +1000

    Updates

---
+++ precache-manifest.7655dd00b9ca04404e40f0219f315d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6584be2e96a327fc33d346bacfd702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242bb64d42804d7f3878",
+    "url": "/static/css/main.79ea6602.chunk.css"
+  },
+  {
+    "revision": "93a53f64b66be29c7110",
+    "url": "/static/js/2.19f15b8a.chunk.js"
+  },
+  {
+    "revision": "242bb64d42804d7f3878",
+    "url": "/static/js/main.90f08ad4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb9227be3fbdce1515e4f80c90d9d339",
+    "url": "/static/media/background1.fb9227be.jpg"
+  }
+]);
