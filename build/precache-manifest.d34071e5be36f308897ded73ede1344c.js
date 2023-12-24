f78d0ed893afd7219f061240604acf6c85451969
gurenko30 <148851531+gurenko30@users.noreply.github.com>
2023-12-25 04:26:16 +1000

    Ваш комментарий к коммиту

---
+++ build/precache-manifest.d34071e5be36f308897ded73ede1344c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57431d22a046fb116dfc7253b00adde",
+    "url": "/Gurenko-portfolio/index.html"
+  },
+  {
+    "revision": "b743f79496b74801fd90",
+    "url": "/Gurenko-portfolio/static/css/main.0452b16a.chunk.css"
+  },
+  {
+    "revision": "93a53f64b66be29c7110",
+    "url": "/Gurenko-portfolio/static/js/2.19f15b8a.chunk.js"
+  },
+  {
+    "revision": "b743f79496b74801fd90",
+    "url": "/Gurenko-portfolio/static/js/main.0a61dbde.chunk.js"
+  },
+  {
+    "revision": "16dc68cf2607ba7f58db",
+    "url": "/Gurenko-portfolio/static/js/runtime~main.c6c62e71.js"
+  },
+  {
+    "revision": "fb9227be3fbdce1515e4f80c90d9d339",
+    "url": "/Gurenko-portfolio/static/media/background1.fb9227be.jpg"
+  }
+]);
